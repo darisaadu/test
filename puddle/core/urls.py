9473c7ec052f5c3077782657ff5a6ea717ec2756
Tanimu Saadu <tanimusaadu2@gmail.com>
2023-12-29 11:56:19 +1300

    updated lot files

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path 
-from .views import index, contact
+from django.contrib.auth import views as auth_views
+from . import views
+from . forms import LoginForm
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
 ]
